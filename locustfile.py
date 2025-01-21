eebba16769848918799dcffd4980989b52064d78
knrd <jvdswi@jadamspam.pl>
2025-01-21 20:39:29 +0100

    nginx config experiments

--- locustfile.py
+++ locustfile.py
@@ -14,35 +14,35 @@ class SharedState:
 class WebTestUser(HttpUser):
     wait_time = between(1, 2)
     test_data = {"message": "test data", "number": 42}
-    shared_state = SharedState()
+    # shared_state = SharedState()
 
-    def get_session_id(self):
-        """Perform login and store session ID"""
-        response = self.client.post(
-            "/login",
-            json={
-                "username": "your_username",
-                "password": "your_password"
-            }
-        )
+    # def get_session_id(self):
+    #     """Perform login and store session ID"""
+    #     response = self.client.post(
+    #         "/login",
+    #         json={
+    #             "username": "your_username",
+    #             "password": "your_password"
+    #         }
+    #     )
         
-        # Assuming the session ID is returned in a cookie or response header
-        # Modify this according to your API's response format
-        self.shared_state.session_id = response.cookies.get('session_id')  # Or response.headers.get('X-Session-ID')
+    #     # Assuming the session ID is returned in a cookie or response header
+    #     # Modify this according to your API's response format
+    #     self.shared_state.session_id = response.cookies.get('session_id')  # Or response.headers.get('X-Session-ID')
         
-        if not self.shared_state.session_id:
-            raise Exception("Failed to get session ID")
+    #     if not self.shared_state.session_id:
+    #         raise Exception("Failed to get session ID")
 
-    def on_start(self):
-        """Called when a User starts"""
-        # Only get the session ID if we haven't retrieved it yet
-        logging.info("EVERY TIME ON START")
-        if not self.shared_state.is_session_id_retrieved:
-            with self.shared_state.lock:  # Use lock to prevent race conditions
-                if not self.shared_state.is_session_id_retrieved:
-                    self.get_session_id()
-                    logging.info("ONLY ONCE")
-                    self.shared_state.is_session_id_retrieved = True
+    # def on_start(self):
+    #     """Called when a User starts"""
+    #     # Only get the session ID if we haven't retrieved it yet
+    #     logging.info("EVERY TIME ON START")
+    #     if not self.shared_state.is_session_id_retrieved:
+    #         with self.shared_state.lock:  # Use lock to prevent race conditions
+    #             if not self.shared_state.is_session_id_retrieved:
+    #                 self.get_session_id()
+    #                 logging.info("ONLY ONCE")
+    #                 self.shared_state.is_session_id_retrieved = True
     
     @tag('flask', 'with-cookie', 'all')
     @task(1)
